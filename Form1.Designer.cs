d063bf17b9111b6ca79bf45abbbb0316a9c1afc1
Soporte <Soporte@DESKTOP-EIO3A0H>
2022-02-07 18:00:12 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnVerificaredad = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(205, 57);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 0;
+            this.txtEdad.TextChanged += new System.EventHandler(this.txtEdad_TextChanged);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(116, 57);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(51, 20);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "Edad";
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(116, 90);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(76, 20);
+            this.lb2.TabIndex = 2;
+            this.lb2.Text = "Mensaje";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(205, 90);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.ReadOnly = true;
+            this.txtMensaje.Size = new System.Drawing.Size(100, 20);
+            this.txtMensaje.TabIndex = 3;
+            // 
+            // btnVerificaredad
+            // 
+            this.btnVerificaredad.Location = new System.Drawing.Point(156, 141);
+            this.btnVerificaredad.Name = "btnVerificaredad";
+            this.btnVerificaredad.Size = new System.Drawing.Size(99, 23);
+            this.btnVerificaredad.TabIndex = 4;
+            this.btnVerificaredad.Text = "Verificar Edad";
+            this.btnVerificaredad.UseVisualStyleBackColor = true;
+            this.btnVerificaredad.Click += new System.EventHandler(this.btnVerificaredad_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(156, 183);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(99, 23);
+            this.btnsalir.TabIndex = 5;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnVerificaredad);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.txtEdad);
+            this.Name = "Form1";
+            this.Text = "Determinar Edad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnVerificaredad;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEdad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificaredad_Click(object sender, EventArgs e)
+        {
+            int edad;
+            edad = Convert.ToInt16(txtEdad.Text);
+            if (edad >= 18)
+            {
+                txtMensaje.Text = "Mayor de Edad";
+            }
+            else
+            {
+                txtMensaje.Text = "Menor de Edad";
+            }
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
